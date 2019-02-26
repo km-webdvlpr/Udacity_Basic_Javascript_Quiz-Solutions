525597549c107bb69623ed4bf336a54ff881f400
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-02-26 17:49:19 +0200

    Add files via upload

---
+++ Datatypes_and_Variables/quiz2_5.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: All Tied Up (2-5)
+Build a single string that resembles the following joke.
+
+Why couldn't the shoes go out and play?
+They were all "tied" up!
+ */
+
+var joke = "knock knock\nknock knock !"
+console.log(joke);
