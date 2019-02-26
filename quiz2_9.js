5a851380397f9ef8b867ba884061e42ecadf492a
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-02-26 17:46:42 +0200

    Delete quiz2_9.js

--- quiz2_9.js
+++
@@ -1,8 +0,0 @@
-/*
- * Programming Quiz: What's my Name? (2-9)
- Create a variable called fullName and assign it your full name as a string.
- */
-
-// your code goes here
-var fullName = "Kabelo Makua"
-console.log(fullName)
