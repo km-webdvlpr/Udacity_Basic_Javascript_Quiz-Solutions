81eba77e24db5bb3519186c87c0fb2a6a7348a60
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-02-26 17:44:39 +0200

    Delete quiz2_1.js

--- quiz2_1.js
+++
@@ -1,9 +0,0 @@
-/*
- * Programming Quiz: First Expression (2-1)
- *
- * Write an expression that uses at least three, different, arithmetic operators
- * to log the number 42 to the console.
- */
-
-// this expression equals 4, change it to equal 42
-console.log(80/2 + 4 - 2);
