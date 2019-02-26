913a33cba10dd5b0bf663067ce0bbcc8ce0c5851
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-02-26 17:02:03 +0200

    Add files via upload

---
+++ quiz2_10.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Out to Dinner (2-10)
+ 
+ Create a variable called bill and assign it the result of 10.25 + 3.99 + 7.15 (don't perform the calculation yourself, let JavaScript do it!). Next, create a variable called tip and assign it the result of multiplying bill by a 15% tip rate. Finally, add the bill and tip together and store it into a variable called total.
+
+Print the total to the JavaScript console.
+
+Hint: 15% in decimal form is written as 0.15_._
+ */
+
+// your code goes here
+var bill = 10.25+3.99 +7.15;
+var tip = bill *.15;
+var total = bill +tip;
+console.log("$"+total.toFixed(2))
---
+++ quiz2_9.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ Create a variable called fullName and assign it your full name as a string.
+ */
+
+// your code goes here
+var fullName = "Kabelo Makua"
+console.log(fullName)
