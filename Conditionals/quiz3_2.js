77a11c0c353014d987ec36f47ba014900d1e552a
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-02-27 09:58:16 +0200

    Add files via upload

---
+++ Conditionals/quiz3_2.js
@@ -0,0 +1,20 @@
+/*
+ * Programming Quiz: Even or Odd (3-2)
+ *
+ * Write an if...else statement that prints `even` if the 
+ * number is even and prints `odd` if the number is odd.
+ *
+ * Note - make sure to print only the string "even" or the string "odd"
+If number equals 1, then odd should be printed to the console.
+If number equals 12, then even should be printed to the console.
+Your Code:
+ */
+
+// change the value of `number` to test your if...else statement
+var number = 13;
+
+if (number%2===0) {
+    console.log('even');
+} else {
+  console.log('odd');
+}
---
+++ Conditionals/quiz3_4.js
@@ -0,0 +1,59 @@
+/*or this quiz, you're going to help solve a fictitious murder mystery that happened here at Udacity! A murder mystery is a game typically played at parties wherein one of the partygoers is secretly, and unknowingly, playing a murderer, and the other attendees must determine who among them is the criminal. It's a classic case of whodunnit.
+
+Since this might be your first time playing a murder mystery, we've simplified things quite a bit to make it easier. Here's what we know! In this murder mystery there are:
+
+four rooms: the ballroom, gallery, billiards room, and dining room,
+four weapons: poison, a trophy, a pool stick, and a knife,
+and four suspects: Mr. Parkes, Ms. Van Cleve, Mrs. Sparr, and Mr. Kalehoff.
+We also know that each weapon corresponds to a particular room, so...
+
+the poison belongs to the ballroom,
+the trophy belongs to the gallery,
+the pool stick belongs to the billiards room,
+and the knife belongs to the dining room.
+And we know that each suspect was located in a specific room at the time of the murder.
+
+Mr. Parkes was located in the dining room.
+Ms. Van Cleve was located in the gallery.
+Mrs. Sparr was located in the billiards room.
+Mr. Kalehoff was located in the ballroom.
+To help solve this mystery, write a combination of conditional statements that:
+
+sets the value of weapon based on the room and
+sets the value of solved to true if the value of room matches the suspect's room
+Afterwards, use this template to print a message to the console if the mystery was solved:
+*/
+
+
+
+// change the value of `room` and `suspect` to test your code
+var room = "gallery";
+var suspect = "Ms. Van Cleve";
+
+var weapon = "";
+var solved = false;
+
+if (room === "gallery") {
+    weapon = "trophy";
+      if (suspect === "Ms. Van Cleve") {
+      solved = true; 
+    }
+} else if (room === "ballroom") {
+    weapon = "poison"; 
+        if (suspect === "Mr. Kalehoff") {
+        solved = true;
+        }
+} else if (room === "billiards room") {
+    weapon = "pool stick";
+        if (suspect === "Mrs. Sparr") {
+        solved = true;
+        }
+} else {
+    weapon = "knife";
+      if (suspect === "Mr. Parkes") {
+      solved = true;
+      }
+}
+if (solved) {
+	console.log(suspect +" did it in the "+ room + " with the "+ weapon +"!");
+}
