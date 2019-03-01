9d4d84c9de2eba7edd69598a580a705acd965388
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-03-01 09:23:54 +0200

    Add files via upload

---
+++ Loops/quiz4_5.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ for (x < 10; x++) {
+  console.log(x);
+}
+ */
+
+// fix the for loop
+
+for (var x = 5; x <= 9 ; x++) {
+  console.log(x);
+}
---
+++ Loops/quiz4_6.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Fix the Error 2 (4-6)
+ 
+ for (var k = 0 k < 200 k++) {
+  console.log(k);
+}
+ */
+
+// fix the for loop
+for (var k = 0; k < 200 ;k++) {
+    console.log(k);
+}
