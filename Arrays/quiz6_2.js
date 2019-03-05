80e08e87c55f17ce6af38b1c6e83808b23a4d946
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-03-05 13:48:32 +0200

    Add files via upload

---
+++ Arrays/quiz6_2.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+var crew = [];
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
+crew [0]=captain;
+crew [1]=second;
+crew [2]=pilot;
+crew [3]=companion;
+crew [4]=mercenary;
+crew [5]=mechanic;
+console.log(crew);
