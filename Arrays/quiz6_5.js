c39801038878e3a287ab4edc65fc33d6f1d39f06
Kabelo Makua <km-webdvlpr@users.noreply.github.com>
2019-03-15 12:34:31 +0200

    Add files via upload

---
+++ Arrays/quiz6_5.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+
+// your code goes here
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
+
+function hasEnoughPlayers(teamArray){
+    return ((teamArray.length>=7)?true : false);
+}
